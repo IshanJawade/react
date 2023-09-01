c9b42d98ad01f858fdbc912188ce97738df4ff64
IshanJawade <ishankjawade@gmail.com>
2023-09-01 17:54:19 +0530

    props added ub cards project

--- cards/src/App.js
+++ cards/src/App.js
@@ -6,9 +6,9 @@ function App(){
         <div>
             <div> Personal Digital Assistants </div>
 
-            <ProfileCard/>
-            <ProfileCard/>
-            <ProfileCard/>
+            <ProfileCard title = "Alexa" handle = "@alexa99"/>
+            <ProfileCard title = "Cortana" handle = "@cortana15"/>
+            <ProfileCard title = "Siri" handle = "@apple.siri"/>
             
         </div>
        
--- cards/src/ProfileCard.js
+++ cards/src/ProfileCard.js
@@ -1,6 +1,18 @@
-function ProfileCard(){
+function ProfileCard({ title , handle }){  //option 3
+
+    // Options 1 
+    // const title = props.title;
+    // const handle = props.handle;
+
+    // Option 2
+    // const { title, handle } = props;
+
     return(
-        <div>Profile Card</div>
+        <div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </div>
+
     );
 }
 

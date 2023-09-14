ec75028f91d52e2830d137e43691efa1251d68ac
IshanJawade <ishankjawade@gmail.com>
2023-09-15 01:05:42 +0530

    local db URI included

--- gofood/backend/db.js
+++ gofood/backend/db.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://ishankjawade:MacBookPro2019@cluster0.kgxjnd5.mongodb.net/gofood?retryWrites=true&w=majority";
-
+//const mongoURI = "mongodb+srv://ishankjawade:MacBookPro2019@cluster0.kgxjnd5.mongodb.net/gofood?retryWrites=true&w=majority";
+const mongoURI = "mongodb://localhost:27017/";
 async function database() {
   await mongoose.connect(mongoURI);
   console.log('DB Connected');
   const fectched_data = await mongoose.connection.db.collection("food_items");
   fectched_data.find({}).toArray(function( err, data){
-        if(err) console.log(err);
-        else console.log(data); 
-        }
-    )
+        err => console.log(err);
+        console.log(data); 
+      }
+      )
 }
-
 module.exports =  database

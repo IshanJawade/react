2b38f3e35edceeb5c3999e24d5dc8e813015fd07
IshanJawade <ishankjawade@gmail.com>
2023-09-15 01:02:31 +0530

    unsuccesssfull attemp to connect db to backend

--- gofood/backend/db.js
+++ gofood/backend/db.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://ishankjawade:MacBookPro2019@cluster0.kgxjnd5.mongodb.net/?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://ishankjawade:MacBookPro2019@cluster0.kgxjnd5.mongodb.net/gofood?retryWrites=true&w=majority";
 
 async function database() {
   await mongoose.connect(mongoURI);
-  console.log('Connected');
+  console.log('DB Connected');
+  const fectched_data = await mongoose.connection.db.collection("food_items");
+  fectched_data.find({}).toArray(function( err, data){
+        if(err) console.log(err);
+        else console.log(data); 
+        }
+    )
 }
 
-
-module.exports =  database;
+module.exports =  database
--- gofood/backend/index.js
+++ gofood/backend/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 4000
 const database = require('./db')
+const fetchData = require('./db')
+
 
 database().catch(err => console.log(err));
 

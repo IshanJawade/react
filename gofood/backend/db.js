b587c084e865206226ab2adc99f6733dc7ddaa42
IshanJawade <ishankjawade@gmail.com>
2023-10-14 11:29:57 +0530

    Data retrived from mongoDB to frontend

--- gofood/backend/db.js
+++ gofood/backend/db.js
@@ -10,8 +10,17 @@ const mongoDB = async () =>{
         console.log("DB connected");
         const fetched_data = mongoose.connection.db.collection("food_items");
         fetched_data.find({}).toArray(function(err, data){
-            if(err) console.log(err);
-            else console.log();
+          const fetched_cat = mongoose.connection.db.collection("food_category");
+          fetched_cat.find({}).toArray(function(err, cat){
+            if(err) {
+              console.log(err)
+            } else {
+              global.food_items = data;
+              global.food_category = cat;
+              // console.log(global.food_category);
+              // console.log(global.food_items);
+            }
+          })
           }
         )
       }
